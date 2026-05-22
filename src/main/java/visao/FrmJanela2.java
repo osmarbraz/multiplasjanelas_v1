f039f68801ccd29424c5f36c5121c0cdf46cbbdd
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2026-05-22 15:04:39 -0300

    Add files via upload

---
+++ src/main/java/visao/FrmJanela2.java
@@ -0,0 +1,45 @@
+package visao;
+
+import java.awt.Rectangle;
+
+/**
+ * @author Osmar de Oliveira Braz Junior
+ */
+public class FrmJanela2 extends javax.swing.JFrame {
+
+    public javax.swing.JButton btJanela1;
+    public javax.swing.JButton btJanela3;
+    private javax.swing.JLabel jLabel1;
+
+    public FrmJanela2() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Janela 2 - Versão 1");
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Janela 2");
+        jLabel1.setBounds(new Rectangle(5, 150, 350, 17));        
+        
+        btJanela1 = new javax.swing.JButton();        
+        btJanela1.setText("Janela 1");
+        btJanela1.setBounds(new Rectangle(5, 250, 90, 27));        
+        
+        btJanela3 = new javax.swing.JButton();
+        btJanela3.setText("Janela 3");
+        btJanela3.setBounds(new Rectangle(160, 250, 90, 27));
+        
+        getContentPane().setLayout(null);
+        getContentPane().add(jLabel1);
+        getContentPane().add(btJanela1);
+        getContentPane().add(btJanela3);
+                
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds((screenSize.width - 416) / 2, (screenSize.height - 338) / 2, 416, 338);
+    }
+}
